5f2c205d9b7f9e45a4ed0b2d63332ddfcdc9d732
Brandon Abrams <brandonabrams@Brandons-MacBook-Pro.local>
2015-10-15 08:02:10 -0400

    js function that puts squares in a level has been finished. Still need to fix css and html

--- project-one/js/app.js
+++ project-one/js/app.js
@@ -1,7 +1,9 @@
 var makeLevelOne = function() {
   for(var i = 0;i < 6; i++){
-    var square = $('<span></span>');
+    var square = $('<div></div>');
+    var idNum = i.toString();
     square.addClass('square');
+    square.attr('id',idNum);
     square.appendTo('.level');
   }
 }
