09c1d4001bf8f5c1e1f20a8ec0bc8eddd31baf1a
Brandon Abrams <brandonabrams@Brandons-MacBook-Pro.local>
2015-10-15 11:47:14 -0400

    first commit on test branch

--- project-one/js/app.js
+++ project-one/js/app.js
@@ -1,9 +1,12 @@
+
+var squares = ['first','second','third','fourth','fith','sixth']
 var makeLevelOne = function() {
   for(var i = 0;i < 6; i++){
     var square = $('<div></div>');
-    var idNum = i.toString();
-    square.addClass('square');
-    square.attr('id',idNum);
+    var sqrNum = squares[i];
+    square.addClass(sqrNum+'square');
+    square.attr('id','square');
     square.appendTo('.level');
   }
 }
+makeLevelOne();
