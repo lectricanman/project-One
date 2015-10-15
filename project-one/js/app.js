ae08f06f5e983c8eb5ead045c8c2c414611113ee
Brandon Abrams <brandonabrams@Brandons-MacBook-Pro.local>
2015-10-15 14:52:39 -0400

    OK, added a function to randomly change 1 square to be red. added a click event to change squares back to white

--- project-one/js/app.js
+++ project-one/js/app.js
@@ -1,12 +1,28 @@
 
-var squares = ['first','second','third','fourth','fith','sixth']
+var Goal = 1000;
+var score = 0;
+var squares = ['first-square','second-square','third-square','fourth-square','fith-square','sixth-square']
 var makeLevelOne = function() {
   for(var i = 0;i < 6; i++){
     var square = $('<div></div>');
     var sqrNum = squares[i];
-    square.addClass(sqrNum+'square');
+    square.addClass(sqrNum);
     square.attr('id','square');
     square.appendTo('.level');
+    $('.'+sqrNum).click(function(){
+      $(this).css('background-color','white');
+      console.log('test');
+    });
   }
 }
+
+
+var makeRed = function(){
+  var sqrNum = squares[Math.floor(Math.random() * (6 - 0)) + 0];
+  $('.'+sqrNum).css('background-color','red');
+}
+
+
+//calling functions
 makeLevelOne();
+//addClick();
