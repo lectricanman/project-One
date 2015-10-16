861377e43893c3cb14ca39668bd91be28c3a8f0d
Brandon Abrams <brandonabrams@Brandons-MacBook-Pro.local>
2015-10-16 11:05:51 -0400

    levelOne processes are automated by start button now we've got to make the timer and goal conditions

--- project-one/js/app.js
+++ project-one/js/app.js
@@ -1,5 +1,7 @@
 
-var Goal = 1000;
+var levelGoal = [1500,2000,2500];
+var currentGoal = 1500;
+var Goal = currentGoal;
 var score = 0;
 var squares = ['square-one','square-two','square-three','square-four','square-five','square-six']
 var makeLevelOne = function() {
@@ -14,30 +16,47 @@ var makeLevelOne = function() {
         $(this).css('background-color','white');
         console.log('up');
         score+= 100;
+        displayScore();
         console.log(score);
       }
        else if(($(this).css('background-color')) ==='rgb(255, 255, 255)'){
          score = score - 100;
+         displayScore();
          console.log(score);
        }
       console.log('click');
     });
   }
 }
-
-var redDelay;
-
 var makeRed = function(){
   var sqrNum = squares[Math.floor(Math.random() * (6 - 0)) + 0];
   $('#'+sqrNum).css('background-color','red');
   window.setTimeout(function(){
     makeWhite(sqrNum);
-  }, 3000);
+  }, 700);
 }
 var makeWhite = function(sqrNum){
    $('#'+sqrNum).css('background-color','white');
  }
-
+var displayScore = function(){
+  $('#player-score').text('score: '+score);
+}
 
 //calling functions
-makeLevelOne();
+
+
+////////////////////
+var playLevelOne = function(){
+  window.setInterval(function(){
+    makeRed();
+  },500);
+}
+
+
+  $('.start').click(function(){
+    console.log('clickstart');
+    makeLevelOne();
+    window.setTimeout(function(){
+      playLevelOne();
+    },1000);
+  });
